88e66e30152a82cd46a8741fd9e5318d6d330fd0
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-11-01 13:51:50 -0500

    debugging

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,12 @@
 const FILES_TO_CACHE = [
-    //'/',
-    '/index.html',
-    '/index.js',
-    '/db.js',
-    '/styles.css',
-    '/manifest.webmanifest',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png'
+    '/',
+    'index.html',
+    'index.js',
+    'db.js',
+    'styles.css',
+    'manifest.webmanifest',
+    'icons/icon-192x192.png',
+    'icons/icon-512x512.png'
   ]
   
   const CACHE_NAME = 'static-cache-v2'
