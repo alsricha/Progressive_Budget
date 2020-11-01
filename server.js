eb35056128fbfbe10081cd6d35d7e39b818cda7f
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-11-01 14:47:59 -0500

    Update to mongoose.connect

--- server.js
+++ server.js
@@ -14,12 +14,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/budget';
-mongoose.connect(MONGODB_URI, {
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+  useFindAndModify: false
 });
 
 // routes
