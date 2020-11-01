8976fc937b86bd50beff0e2905bf28cd75e037a2
Alex Skudrovskis <alex.skudrovskis@gmail.com>
2020-11-01 09:40:34 -0500

    function to check database

--- public/db.js
+++ public/db.js
@@ -18,5 +18,29 @@ request.oneerror = function (event) {
     console.log("Sorry!" + event.target.errorCode);
 };
 
-
+function checkDatabase() {
+    const transaction = db.transaction(["pending"], "readwrite");
+    const store = transaction.objectStore("pending");
+    const getAll = store.getAll();
+  
+    getAll.onsuccess = function () {
+      if (getAll.result.length > 0) {
+        fetch("/api/transaction/bulk", {
+          method: "POST",
+          body: JSON.stringify(getAll.result),
+          headers: {
+            Accept: "application/json, text/plain, */*",
+            "Content-Type": "application/json"
+          }
+        })
+          .then(response => response.json())
+          .then(() => {
+            // delete records if successful
+            const transaction = db.transaction(["pending"], "readwrite");
+            const store = transaction.objectStore("pending");
+            store.clear();
+          });
+      }
+    };
+  }
 
